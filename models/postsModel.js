533dbf51a6a42a91b335106a54d4cfedeb98d01e
Yumi <a13158y@gmail.com>
2021-12-07 16:01:56 -0800

    wip

--- models/postsModel.js
+++ models/postsModel.js
@@ -2,19 +2,26 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const postsSchema = new Schema({
-  userId: {
-    type: String,
-    required: true,
+const postsSchema = new Schema(
+  {
+    userId: {
+      type: String,
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+    imageUrl: [
+      {
+        type: String,
+        required: true,
+      },
+    ],
   },
-  content: {
-    type: String,
-    required: true,
-  },
-  imageUrl: [{
-    type: String,
-    required: true,
-  }],
-});
+  {
+    timestamps: true,
+  }
+);
 
 module.exports = mongoose.model("Posts", postsSchema);
