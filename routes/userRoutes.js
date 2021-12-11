e63e8e4205555eda8b436b70ce5b634f4685204b
EricTW-91 <amperic05@gmail.com>
2021-12-10 17:23:36 -0800

    friend api

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -29,7 +29,7 @@ router.get('/getUser/:id', async (req, res) => {
 // Update user Info
 router.post('/editUser/:id', async (req, res)=>{
     try {
-            const data = await User.findOneAndUpdate({_id: req.params.id}, {
+            const data = await User.updateOne({_id: req.params.id}, {
             firstName: req.body.firstName,
             lastName: req.body.lastName,
             userName: req.body.userName,
@@ -49,6 +49,116 @@ router.post('/editUser/:id', async (req, res)=>{
     }
 })
 
+// Send a friend request
+router.put('/friendRequest', async (req, res) => {
+    try {
+        const sentReqData = await User.updateOne({_id: req.body.senderID}, {
+            $push: {
+                "friends.sentRequest": { userID: req.body.receiverID }
+            }
+        })
+        const requestData = await User.updateOne({_id: req.body.receiverID}, {
+            $push: {
+                "friends.request": { userID: req.body.senderID }
+            }
+        })
+
+        return res.status(201).json({
+            message: 'Friend request updated',
+            data: {
+                sender: sentReqData,
+                receiver: requestData
+            }
+        });
+    }
+    catch (error) {
+        return res.status(500).json({
+            message: error.message,
+        })
+    }
+})
+
+// Reject a friend request
+router.put('/friendReject', async (req, res) => {
+    try {
+        const sentReqData = await User.updateOne({_id: req.body.senderID}, {
+            $pull: {
+                "friends.sentRequest": { userID: req.body.receiverID }
+            }
+        })
+        const requestData = await User.updateOne({_id: req.body.receiverID}, {
+            $pull: {
+                "friends.request": { userID: req.body.senderID }
+            }
+        })
+
+        return res.status(201).json({
+            message: 'Friend reject updated',
+            data: {
+                sender: sentReqData,
+                receiver: requestData
+            }
+        });
+    }
+    catch (error) {
+        return res.status(500).json({
+            message: error.message,
+        })
+    }
+})
+// Accept a friend request
+router.put('/friendAccept', async (req, res) => {
+    try {
+        const sentReqData = await User.updateOne({_id: req.body.senderID}, {
+            $pull: {
+                "friends.sentRequest": { userID: req.body.receiverID }
+            }
+        })
+        const requestData = await User.updateOne({_id: req.body.receiverID}, {
+            $pull: {
+                "friends.request": { userID: req.body.senderID }
+            }
+        })
+        const senderData = await User.updateOne({_id: req.body.senderID}, {
+            $push: {
+                "friends.friendsList": { userID: req.body.receiverID }
+            }
+        })
+        const receiverData = await User.updateOne({_id: req.body.receiverID}, {
+            $push: {
+                "friends.friendsList": { userID: req.body.senderID }
+            }
+        })
+
+        return res.status(201).json({
+            message: 'Friend accept updated',
+            data: {
+                sender: sentReqData,
+                receiver: requestData
+            }
+        });
+    }
+    catch (error) {
+        return res.status(500).json({
+            message: error.message,
+        })
+    }
+})
+// Get friend list
+router.get('/getFriends', async (req, res) => {
+    try {
+        const data = await User.findById(req.body.id)
+
+        return res.status(201).json({
+            message: 'Get friend list',
+            data
+        })
+    } catch (error) {
+        return res.status(500).json({
+            message: error.message,
+        })
+    }
+})
 
 
 module.exports = router;
