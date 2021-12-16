1a7790a3a97132747589a9e1f7b64170e203a42f
EricTW-91 <amperic05@gmail.com>
2021-12-16 02:04:35 -0800

    updated with client

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -14,7 +14,6 @@ router.route('/resetpassword/:userId/:token').post(resetPassword)
 
 // Fetch user data
 router.get('/getUser/:id', async (req, res) => {
-        
     try {
         const user = await User.findById(req.params.id)
 
