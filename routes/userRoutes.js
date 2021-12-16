2a54e39da72ff4ef5a66852ebf01322972df1fed
Yumi <a13158y@gmail.com>
2021-12-16 14:10:09 -0800

    fix a bug

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,35 +1,55 @@
-const express = require('express');
+const express = require("express");
 
-const { registerUser, authUser, resetPassword, forgotPassword, findUser } = require('../controllers/userControllers');
-const authToken = require('../middlewares/authToken')
-const User = require('../models/userModel')
-const Posts = require('../models/postsModel')
-
+const {
+  registerUser,
+  authUser,
+  resetPassword,
+  forgotPassword,
+  findUser,
+} = require("../controllers/userControllers");
+const authToken = require("../middlewares/authToken");
+const User = require("../models/userModel");
+const Posts = require("../models/postsModel");
 
 const router = express.Router();
 
-router.route('/').post(registerUser);
-router.route('/login').post(authUser);
-router.route('/resetpassword').post(forgotPassword)
-router.route('/resetpassword/:userId/:token').post(resetPassword)
-router.route('/explore').post(findUser)
+router.route("/").post(registerUser);
+router.route("/login").post(authUser);
+router.route("/resetpassword").post(forgotPassword);
+router.route("/resetpassword/:userId/:token").post(resetPassword);
+router.route("/explore").post(findUser);
 
 // Fetch user data
-router.get('/getUser/:id', async (req, res) => {
-    try {
-        const user = await User.findById(req.params.id)
+router.get("/getUser/:id", async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
 
-        return res.status(201).json({
-            message: 'Successfully fetch the user data',
-            data: user
-        });
-    }
-    catch (error) {
-        return res.status(500).json({
-            message: error.message,
-        })
-    }
-})
+    return res.status(201).json({
+      message: "Successfully fetch the user data",
+      data: user,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: error.message,
+    });
+  }
+});
+
+// Fetch all user data
+router.get("/getUser", async (req, res) => {
+  try {
+    const user = await User.find();
+
+    return res.status(201).json({
+      message: "Successfully fetch the user data",
+      data: user,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: error.message,
+    });
+  }
+});
 
 // Update user Info
 router.post("/editUser/:id", async (req, res) => {
@@ -57,39 +77,44 @@ router.post("/editUser/:id", async (req, res) => {
 });
 
 // Send a friend request
-router.put('/friendRequest', async (req, res) => {
-    try {
-        const getSender = await User.findById(req.body.senderID)
+router.put("/friendRequest", async (req, res) => {
+  try {
+    const getSender = await User.findById(req.body.senderID);
 
-        const sentReqData = await User.updateOne({_id: req.body.senderID}, {
-            $push: {
-                "friends.sentRequest": { userID: req.body.receiverID }
-            }
-        })
-        const requestData = await User.updateOne({_id: req.body.receiverID}, {
-            $push: {
-                "friends.request": {
-                    userID: req.body.senderID,
-                    userName: getSender.userName,
-                    avatarUrl: getSender.avatarUrl
-                }
-            }
-        })
+    const sentReqData = await User.updateOne(
+      { _id: req.body.senderID },
+      {
+        $push: {
+          "friends.sentRequest": { userID: req.body.receiverID },
+        },
+      }
+    );
+    const requestData = await User.updateOne(
+      { _id: req.body.receiverID },
+      {
+        $push: {
+          "friends.request": {
+            userID: req.body.senderID,
+            userName: getSender.userName,
+            avatarUrl: getSender.avatarUrl,
+          },
+        },
+      }
+    );
 
-        return res.status(201).json({
-            message: 'Friend request updated',
-            data: {
-                sender: sentReqData,
-                receiver: requestData
-            }
-        });
-    }
-    catch (error) {
-        return res.status(500).json({
-            message: error.message,
-        })
-    }
-})
+    return res.status(201).json({
+      message: "Friend request updated",
+      data: {
+        sender: sentReqData,
+        receiver: requestData,
+      },
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: error.message,
+    });
+  }
+});
 
 // Reject a friend request
 router.put("/friendReject", async (req, res) => {
@@ -222,21 +247,19 @@ router.put("/friendRemove", async (req, res) => {
 });
 
 // Fetch comment user
-router.post('/getCommentUser', async (req, res) => {
-    try {
-        const users = await User.find({ _id: { $in: req.body.users } })
-         
+router.post("/getCommentUser", async (req, res) => {
+  try {
+    const users = await User.find({ _id: { $in: req.body.users } });
 
-        return res.status(201).json({
-            message: 'Successfully fetch the user data',
-            data: users
-        });
-    }
-    catch (error) {
-        return res.status(500).json({
-            message: error.message,
-        })
-    }
-})
+    return res.status(201).json({
+      message: "Successfully fetch the user data",
+      data: users,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: error.message,
+    });
+  }
+});
 
 module.exports = router;
